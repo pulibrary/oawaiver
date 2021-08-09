0d47027e01b2f98abceeab6b40d7dae334d52d47
jrgriffiniii <jrgriffiniii@gmail.com>
2021-08-09 13:18:55 -0400

    [WIP]

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ def set_branch
   return branch unless branch == "master"
   "main"
 end
-set :branch, set_branch
+set :branch, 'jrgriffiniii-fix-deployments'
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
