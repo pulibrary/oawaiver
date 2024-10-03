4d2763b9d0d4309faef45044153e98ecc1f856b1
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2024-10-03 14:52:47 -0400

    Ensuring that the latest patch release for 12.3 of health-monitor-rails is used; Using the Solr configuration instead of the custom `checks` class

--- config/initializers/health_monitor.rb
+++ config/initializers/health_monitor.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 Rails.application.config.after_initialize do
   HealthMonitor.configure do |config|
-    # config.cache
+    config.cache
 
-    config.add_custom_provider(SolrStatus)
+    config.solr.configure do |c|
+      url = Sunspot.config.solr.url
+      uri = URI(url)
+      status_uri = URI("#{uri.scheme}://#{uri.hostname}:#{uri.port}/solr/admin/cores?action=STATUS")
 
-    # Make this health check available at /health
+      c.url = status_uri.to_s
+    end
+
     config.path = :health
 
-    # config.error_callback = proc do |e|
-    #  Rails.logger.error "Health check failed with: #{e.message}"
-    #  Honeybadger.notify(e)
-    # end
+    config.error_callback = proc do |e|
+      Rails.logger.error "Health check failed with: #{e.message}"
+      Honeybadger.notify(e)
+    end
   end
 end
