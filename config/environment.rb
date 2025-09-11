ca302acb505702bcd832f36b84d0e9f91ed6af8d
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-09-11 10:10:34 -0400

    wip

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,11 @@
 # Load the Rails application.
 require_relative "application"
 
+begin
 # Initialize the Rails application.
-Rails.application.initialize!
+  Rails.application.initialize!
+rescue StandardError => e
+  #puts "Error during initialization: #{e.message}"
+  puts e.backtrace
+  raise e
+end
