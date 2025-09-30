81e8735e9eacc52cce3ea71b55d84e5fcb6e9190
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-09-30 11:06:55 -0400

    Upgrading to Rails 8

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Allow default test hosts to bypass HostAuthorization in Rails 8
+  config.hosts.clear
+  config.middleware.delete ActionDispatch::HostAuthorization
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
